5e58bc3a93191a63e7850684b81471d2f266d695
Thidaporn Kijkamjai <jinglejill@thidaporns-macbook-pro.local>
2017-12-08 17:54:41 +0700

    Initial Commit

---
+++ Dorota/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Dorota
+//
+//  Created by Thidaporn Kijkamjai on 12/8/2560 BE.
+//  Copyright © 2560 Appxelent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Dorota/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Dorota
+//
+//  Created by Thidaporn Kijkamjai on 12/8/2560 BE.
+//  Copyright © 2560 Appxelent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
